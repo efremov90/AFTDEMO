f6e3cf88bfe13b2888e1a9ed6e56472fc68f22f5
NEO <a>
2020-03-22 18:42:13 +0300

    DemoDataBase

--- src/main/java/DemoDataBase.java
+++ src/main/java/DemoDataBase.java
@@ -13,16 +13,21 @@ http://www.h2database.com/html/main.html
 3. Также необходимо добавить драйвер h2 C:\Program Files (x86)\H2\bin файл-jar в classpath
 4. Теория https://www.examclouds.com/ru/java/java-core-russian/lesson22*/
 public class DemoDataBase {
+    //Параметры подключения
     public static final String DRIVER = "org.h2.Driver";
     public static final String DB = "test";
     public static final String URL = "jdbc:h2:~/"+DB;
     public static final String USER = "sanya";
     public static final String PASSWORD = "12345";
 
+    //Запросы
+    //Чтобы последующим людям не создавать схему и таблицу, не наполнять таблицу
+    //Прописал эти запросы тут. Т.е. их создание осуществляется динамически при выполнении программы
     private static final String CREATE_SCHEMA_DEMO="create schema DEMO;";
 
     private static final String DROP_SCHEMA_DEMO="drop schema DEMO";
 
+    //приходится везде дописывать имя схемы, почему-то без неё не работает
     private static final String CREATE_TABLE_PERSON="create table DEMO.PERSON " +
             "(PERSON_ID NUMBER(5) auto_increment," +
             "FIRSTNAME VARCHAR2(255)," +
@@ -41,21 +46,27 @@ public class DemoDataBase {
             "insert into DEMO.PERSON (FIRSTNAME,LASTNAME,GENDER) " +
             "values ('Ольга','Ефремова','FEMALE');";
 
+    //
     public static void main (String[] args) {
         try {
+            //Соединение
             Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
             Statement stmt = conn.createStatement();
+            //Создание и наполнение
             stmt.executeUpdate(CREATE_SCHEMA_DEMO);
             stmt.executeUpdate(CREATE_TABLE_PERSON);
             stmt.executeUpdate(INSERT_DEFAULT_PERSON);
+            //Выборка
             ResultSet rs = stmt.executeQuery("SELECT * FROM DEMO.PERSON");
             while (rs.next()) {
                 System.out.println(rs.getString("FIRSTNAME")+" "
                         +rs.getString("LASTNAME")+" "
                         +GenderType.valueOf(rs.getString("GENDER")).getDescription());
             }
+            //Удаление
             stmt.executeUpdate(DROP_TABLE_PERSON);
             stmt.executeUpdate(DROP_SCHEMA_DEMO);
+            //Закрытие соединения
             conn.close();
         }
         catch (Exception e) {
