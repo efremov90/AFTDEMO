e3ff88f832b4ce3b9571a90feebb86784b3fcdff
Olga <olechin1990@yandex.ru>
2020-03-23 17:45:47 +0300

    change2

---
+++ src/main/java/Transport.java
@@ -0,0 +1,19 @@
+package main.java;
+
+public class Transport {
+    int passengers, oilvolume, oilprokm;
+    // Конструктор
+    Transport(int aa, int bb, int cc) {
+        passengers=aa;
+        oilvolume=bb;
+        oilprokm=cc;
+    }
+    // Возвратить дальность транспорта
+    int distance (){
+        return oilvolume*oilprokm;
+    }
+    // Рассчитать объем топлива для траспорта для N км
+    double oilrequired(int kilometers) {
+        return (double) kilometers/oilprokm;
+    }
+}
