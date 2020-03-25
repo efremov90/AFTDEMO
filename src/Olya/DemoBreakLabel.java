00b5e66d55f32130bd9bfb57e0ca39d7e8cf2de4
Olga <olechin1990@yandex.ru>
2020-03-26 02:12:38 +0300

    added the class "DemoBreakLabel"

---
+++ src/Olya/DemoBreakLabel.java
@@ -0,0 +1,18 @@
+package Olya;
+
+public class DemoBreakLabel {
+    public static void main (String[] args) {
+        label:
+        for (int i = 0; i < 10; i++) {
+            if (i==6) {
+                break;
+            }
+            System.out.print(i + " : ");
+            for (int j=10; j < 15; j++) {
+                System.out.print(j + " ");
+                break label;
+            }
+            System.out.println();
+        }
+    }
+}
