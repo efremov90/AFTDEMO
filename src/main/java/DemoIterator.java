cf4b5774ca50033e55a11fc206950b84e7c7fc69
NEO <a>
2020-04-09 00:14:23 +0300

    DemoLyambda значитель пополен + еще осталось

---
+++ src/main/java/DemoIterator.java
@@ -0,0 +1,57 @@
+package main.java;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class DemoIterator {
+    public static void main (String[] args) {
+
+        //https://juja.com.ua/java/java-collections/iterate-arraylist-java/
+        List<String> colors = Arrays.asList("red", "yellow");
+
+        System.out.println("Basic loop:");
+        for (int i = 0; i < colors.size(); i++) {
+            String color = colors.get(i);
+            System.out.println("color: "+color);
+        }
+        System.out.println();
+
+        System.out.println("foreach:");
+        for (String color : colors) {
+            System.out.println("color: "+color);
+        }
+        System.out.println();
+
+        System.out.println("Basic loop with iterator:");
+        for (Iterator<String> it = colors.iterator(); it.hasNext(); ) {
+            String color = it.next();
+            System.out.println("color: "+color);
+        }
+        System.out.println();
+
+        //https://metanit.com/java/tutorial/5.10.php
+        //https://hr-vector.com/java/iterator
+        //https://www.examclouds.com/ru/java/java-core-russian/iterator
+        System.out.println("Iterator with while loop:");
+        Iterator<String> it = colors.iterator();
+        while (it.hasNext()) {
+            String color = it.next();
+            System.out.println("color: "+color);
+        }
+        System.out.println();
+
+        System.out.println("JDK 8 streaming example lambda expression:");
+        colors.stream().forEach(color -> System.out.println("color: "+color));
+        System.out.println();
+
+        System.out.println("JDK 8 streaming example method reference:");
+        colors.stream().forEach(System.out::println);
+
+        // JDK 8 for each with lambda
+//        colors.forEach(color -> printItemList(color));
+
+        // JDK 8 for each
+//        colors.forEach(ListExample::printItemList);
+    }
+}
--- src/main/java/DemoLyambda.java
+++ src/main/java/DemoLyambda.java
@@ -1,7 +1,6 @@
 package main.java;
 
-import java.util.function.Consumer;
-import java.util.function.Predicate;
+import java.util.function.*;
 
 /*Лямбда https://www.examclouds.com/ru/java/java-core-russian/lambda-russian
 Прочитать суть и синтаксис + функциональные интерейфесы
@@ -20,28 +19,71 @@ public class DemoLyambda {
     public static void main (String [] args) {
 
         //Интерфейс Predicate https://www.examclouds.com/ru/java/java-core-russian/predicate-interface-russian
+        //Принимает на вход значение, проверяет состояние и возвращает boolean значение в качестве результата.
         System.out.println("Интерфейс Predicate:x>0:");
-        Predicate<Integer> isPositive = x->x>0;
+        Predicate<Integer> isPositive = x -> {
+            //м.б. произвольная логика
+            //важно, чтобы в итоге возвращалось return логическое boolean значение
+            //если всё обходится одним выражением, то можно писать короче, например, x -> x>0;
+            return x>0;
+        };
         System.out.println(isPositive.test(7));
         System.out.println(isPositive.test(-7));
 
+        //Существуют бинарные специализации интерфейсов Predicate, Consumer, Function
+        // https://www.examclouds.com/ru/java/java-core-russian/functional-interface-russian
+        System.out.println("Интерфейс BiPredicate:x>y:");
+        BiPredicate<Integer,Integer> xOverY = (x, y) -> (x > y);
+        System.out.println(xOverY.test(2,1));
+        System.out.println(xOverY.test(1,2));
+
+        //Predicate интерфейс имеет методы and, or, negate
         System.out.println("Интерфейс Predicate:contains:");
         Predicate<String> containsA = t -> t.contains("A");
-        Predicate<String> containsB = t -> t.contains("C");
-        System.out.println(containsA.and(containsB).test("ABCD"));
-        System.out.println(containsA.and(containsB).test("ABD"));
+        Predicate<String> containsC = t -> t.contains("C");
+        //Если перейти в реализацию and, то можно увидеть return (t) -> test(t) && other.test(t),
+        //т.е. containsA.and(containsC).test("ABCD") эквивалентен
+        //containsA.test("ABCD") && containsC.test("ABCD")
+        System.out.println(containsA.and(containsC).test("ABCD"));
+        System.out.println(containsA.and(containsC).test("ABD"));
         System.out.println();
 
-        //Интерфейс Consumer
-        System.out.println("Интерфейс Consumer:sout:");
-        Consumer<String> sout = (s)->{
-            //м.б. произвольная логика
+        //Интерфейс Consumer https://www.examclouds.com/ru/java/java-core-russian/consumer-interface-russian
+        //Consumer интерфейс используется в случае, если необходимо передать объект на вход
+        // и произвести над ним некоторые операции не возвращая результат.
+        System.out.print("Интерфейс Consumer:sout: ");
+        Consumer<String> sout = (s) -> {
+            //м.б. произвольная логика, необязательно вывод
+            //если всё обходится одним выражением, то можно писать короче, например, (s) -> System.out.println(s);
             System.out.println(s);
         };
         String str = "Hello world!";
         sout.accept(str);
         System.out.println("Обычный System.out.println: "+str);
 
-        //Остальные интерфейсы не стал приводить, можно просто почитать
+        //Интерфейс Supplier https://www.examclouds.com/ru/java/java-core-russian/supplier-interface-russian
+        //Интерфейс Supplier используется тогда, когда на вход не передаются значения, но необходимо вернуть результат.
+        System.out.print("Интерфейс Consumer:getStringHelloWorld: ");
+        String sHello = "Hello";
+        String sWorld = "World";
+        Supplier<String> getStringHelloWorld = () -> {
+            //м.б. произвольная логика
+            //важно, чтобы в итоге возвращалось return логическое выражение
+            //если всё обходится одним выражением, то можно писать короче, например, () -> sHello+" "+sWorld+"!";
+            return sHello+" "+sWorld+"!";
+        };
+        System.out.println(getStringHelloWorld.get());
+
+        //Интерфейс Function https://www.examclouds.com/ru/java/java-core-russian/function-interface-russian
+        //Принимает значение в качестве аргумента одного типа и возвращает другое значение.
+        // Часто используется для преобразования одного значения в другое
+        System.out.print("Интерфейс Consumer:getStringHelloWorld: ");
+        Function<Integer, Integer> increaseBy10 = (t) -> {
+            //м.б. произвольная логика
+            //важно, чтобы в итоге возвращалось return логическое выражение
+            //если всё обходится одним выражением, то можно писать короче, например, () -> "Hello World!";
+            return t+10;
+        };
+        System.out.println(increaseBy10.apply(5));
     }
 }
