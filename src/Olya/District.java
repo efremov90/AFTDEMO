a305af1724694b8a61c52366cc7f4323f64bb570
Olga <olechin1990@yandex.ru>
2020-04-09 17:18:24 +0300

    added class "DemoOOP" and classes "Country" --> "City" --> "District" (via 'extends')

---
+++ src/Olya/District.java
@@ -0,0 +1,15 @@
+package Olya;
+
+public class District extends City {
+    String districtName;
+
+    public District (String countryName, double population, double square, String language, String cityName, String districtName) {
+        super (countryName, population, square, language, cityName);
+        this.districtName = districtName;
+    }
+
+    public void aboutMe (){
+        System.out.println("I live in district '" + districtName + "'. It's a part of " + super.getCityName());
+    }
+
+}
