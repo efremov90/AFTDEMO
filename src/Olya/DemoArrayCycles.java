3a4cb32f07503c8809466a39e912beb862e58741
Olga <olechin1990@yandex.ru>
2020-05-06 13:31:18 +0300

    Дописала DemoArrayCycles, добавила ещё и "ленивый" (просто попробовала) метод №2 для вывода положительных чисел в массиве. Добавила toBinary. Дальше дополню класс, когда перейду к изучению исключений.

--- src/Olya/DemoArrayCycles.java
+++ src/Olya/DemoArrayCycles.java
@@ -33,6 +33,8 @@ public static int [] fibonachi (int n) {
       return f;
 }
 
+// Массив положительных чисел
+    //Способ №1
 public static int []  toArraypositiveAsk (int [] a){
       int countPosotive = 0;
       int[] b= a.clone();
@@ -58,6 +60,45 @@ public static int []  toArraypositiveAsk (int [] a){
       return arrayPosit;
 }
 
+// Способ №2 "для ленивых"
+    public static void positive (int [] c) {
+      int [] e = c.clone();
+      Arrays.sort(e);
+      for (int k=0; k<e.length; k++) {
+          if (e[k]>0){
+              System.out.print(e[k] + " ");
+          }
+      }
+    }
+
+    public static int closeTo5(int[] a) {
+        int n=5;
+        //помним, что у Array нумерация начинается с 0
+        int close_to_a=a[0];
+        for (int i = 0; i < a.length; i++) {
+            if ((Math.abs(a[i]-n)<=Math.abs(close_to_a-n))&&(a[i]>close_to_a)) {
+                close_to_a=a[i];
+            }
+        }
+        return close_to_a;
+    }
+
+    public static String toBinary (int number){
+      String str="";
+      long g = number;
+      if (number==0) {
+          return "0";
+      }
+      while ((g/2)>=1) {
+          str=(g%2)+str;
+          g=Math.round(Math.floor(g/2));
+      }
+      if (g==1){
+          str=g+str;
+      }
+      return str;
+    }
+
 
     public static void main(String[] args) {
       int n=7;
@@ -68,6 +109,25 @@ public static int []  toArraypositiveAsk (int [] a){
 
         int f = 10;
         System.out.println("Факториал 10: " + factorial(f));
+
+        // способ №2
+        int [] array1 = {5, 12, -10, 4, -99, 18, 0};
+        System.out.println("Имеется массив: " + Arrays.toString(array1));
+        System.out.println("Выводим отсортированный массив, содержащий только положительные числа: ");
+        positive(array1);
+        System.out.println();
+
+        // способ №1
+        int [] array2 = { -88, -4, -99, 1, 7, 0};
+        int [] array3 = toArraypositiveAsk(array2);
+        System.out.println("Первый способ: " + Arrays.toString(array3));
+
+
+        System.out.println("Ближайшее к 5 число из массива array2: " + closeTo5(array2));
+
+        int z= 55;
+        System.out.println("Переводим " + z + " в бинарное представление: " + toBinary(z));
+
 
     }
 }
